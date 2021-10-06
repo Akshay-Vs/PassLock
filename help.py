123f7ab6ab4d59f64563f7ae02bfe951efab0846
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2021-10-06 17:29:02 +0530

    Add files via upload

--- help.py
+++ help.py
@@ -1,7 +1,7 @@
 from addons import *
 def help():
     clear()
-    cprint("""\tCommands\n\n'new' to create new password\n'show' to show saved passwords\n\n--del to delete password\n--move to move passwords from default folder\n--copy to copy saved passwords\n--mkrecovery to save current state\n--p to change directory path\n--list to list all password IDs\n--Exit to exit console\n-c to copy password to clipboard\n--r n to generate a random password with n length, eg: --r (50) will generate a 40 bit long password.Default is 40""","cyan",attrs=["bold"])
+    cprint("""\tCommands\n\n'new' to create new password\n'show' to show saved passwords\n\n--del to delete password\n--move to move passwords from default folder\n--copy to copy saved passwords\n--mkrec to save current state\n--p to change directory path\n--list to list all password IDs\n--Exit to exit console\n-c to copy password to clipboard\n--r n to generate a random password with n length, eg: --r (50) will generate a 40 bit long password.Default is 40""","cyan",attrs=["bold"])
     typing('''\nRecomended way to store passwords 
     path(C:\directory\data)
                |

01f9278ac8addf3e383fcb60d6d16b68755c1386
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2021-11-04 18:57:06 +0530

    console

--- data/snippets/cpp/resources/console.lua
+++ data/snippets/cpp/resources/console.lua
@@ -1,16 +1,11 @@
 local LuaConsole = CreateFrame("Frame", "LuaConsole", UIParent)
 local inset = 3
-
 BINDING_HEADER_LUACONSOLE = "LuaConsole"
 BINDING_NAME_LUACONSOLE = "New LuaConsole"
-
--- Just a gimmick
 local user = UnitName("Local Console").."@"..GetRealmName()
-
 local prefix
 local keywords = {}
 LuaConsole.KeyWords = keywords
 local currTable, currTableName
-
 local typeFormat = {}
 LuaConsole.TypeFormat = typeFormat

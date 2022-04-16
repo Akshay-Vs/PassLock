8490a383e28af2c09bbf521bdb9a80ec239f389a
Akshay Vs <85959609+Akshay-Vs@users.noreply.github.com>
2022-04-16 22:51:56 +0530

    Not important

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-password_placeholder = theme.placeholder
-max_index=theme.max_index
-warn=theme.color_warn
-error=theme.color_error
-primary_color=theme.color_primary
-secondary=theme.color_secondary
